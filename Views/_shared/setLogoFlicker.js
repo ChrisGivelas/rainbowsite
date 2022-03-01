a5460684d21c5b6ce1a9be1f229b6fdaf0969414
Chris Givelas <chris_g_50@hotmail.com>
2022-02-28 22:15:17 -0500

    Redesign Footer, and include tripadvisor and restaurant-guru widgets

--- Views/_shared/setLogoFlicker.js
+++ Views/_shared/setLogoFlicker.js
@@ -1,20 +1,10 @@
 window.onload = function () {
-    setLogoFlicker();
-};
+    var logo = document.getElementById("logo");
 
-function setLogoFlicker() {
-    var logo = document.getElementById("LOGO");
-
-    $(logo).animate({ opacity: 0.0 }, 150);
-    $(logo).animate({ opacity: 1.0 }, 150);
-    $(logo).animate({ opacity: 0.0 }, 150);
-    $(logo).animate({ opacity: 1.0 }, 1500);
-
-    var interval = 5000;
     setInterval(function () {
         $(logo).animate({ opacity: 0.0 }, 150);
         $(logo).animate({ opacity: 1.0 }, 150);
         $(logo).animate({ opacity: 0.0 }, 150);
         $(logo).animate({ opacity: 1.0 }, 1500);
-    }, interval);
-}
+    }, 10000);
+};
